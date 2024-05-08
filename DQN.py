d90aa334ea9d16cc522d4fddb331d805d44b90ae
Nick Hofstetter <nickhofstetter99@gmail.com>
2024-05-08 21:48:45 +0200

    started with the training loop

--- DQN.py
+++ DQN.py
@@ -1,4 +1,8 @@
 from torch import nn
+import random
+import numpy as np
+import gymnasium as gym
+
 
 class Qfunction(nn.Module):
     def __init__(self, input_dim, hidden_dim, output_dim):
@@ -8,8 +12,46 @@ class Qfunction(nn.Module):
             nn.ReLU(),
             nn.Linear(hidden_dim, hidden_dim),
             nn.ReLU(),
-            nn.Linear(hidden_dim, output_dim)
+            nn.Linear(hidden_dim, output_dim),
         )
+    
+    def forward(self, x):
+        outputs = self.stack(x)
+        return outputs
+    
+
+
+def train(network, env):
+    # initialize replay buffer
+    replay_buffer = []
+
+    num_episodes = 10
+    T = 10
+    epsilon = 0.2
+    for episode in range(num_episodes):
+        #initialise sequence
+        current_state = env.reset()
+
+        for t in range(T):
+            # select an action
+            sample = random.uniform(0,1)
+            if sample < epsilon:
+                # ranodm action
+                action = env.action_space.sample()
+            else:
+                # action that maximizes the Q function
+                q_values = network(current_state)
+                action = np.argmax(q_values)
+
+            # execute the action
+            next_state, reward, terminated, truncated, info = env.step(action)
+
+            # update replay buffer
+            replay_buffer.append(current_state, action, reward, next_state)
+
+            # sample minibatch from buffer
+            
+
 
 
 # network for Q function
